13fc71b711a47b7b99bfb981c37560a81f503d5b
Yaremith  Damian <laudamian27@gmail.com>
2020-01-31 21:21:53 -0600

    Initial Commit

---
+++ practica/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practica
+//
+//  Created by Laura Yaremith Damian  Padilla on 31/01/20.
+//  Copyright © 2020 Laura Yaremith Damian  Padilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
